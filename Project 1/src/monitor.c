772200cd227d16d47cd5c8eb17a8070c8a8f5b2d
Henrique Ferrolho <henriqueferrolho@gmail.com>
2014-03-20 19:16:54 +0000

    wrong no of args prevention added

--- Project 1/src/monitor.c
+++ Project 1/src/monitor.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
+	if (argc <= 3) {
+		printf("Wrong number of arguments.\n");
+		printf("Usage: monitor <time> <word> <files>\n");
+		exit(-1);
+	}
+
 	printf("tempo: %s\n", argv[1]);
 	printf("palavra: %s\n", argv[2]);
 

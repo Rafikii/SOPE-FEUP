ef353ed3c7ffcfd134ea34cc9d4c014d450f6d44
Rafiki <rafaelapintofaria@gmail.com>
2014-03-20 19:05:56 +0000

    Added tests

--- Project 1/src/monitor.c
+++ Project 1/src/monitor.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-	printf("hello\n");
+int main(int argc, char** argv) {
+	printf("tempo: %s\n", argv[1]);
+	printf("palavra: %s\n", argv[2]);
+
+	int i;
+	for(i = 3; i < argc; i++) {
+		printf("ficheiro %d: %s\n", i-2, argv[i]);
+	}
+
+	printf("Done.\n");
 	return 0;
 }

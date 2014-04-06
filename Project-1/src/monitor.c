ef5029aab13a279a14f31fc72e0947d38e7bd780
Rafiki <rafaelapintofaria@gmail.com>
2014-04-06 01:01:50 +0100

    Added getTimeToString()

--- Project-1/src/monitor.c
+++ Project-1/src/monitor.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <time.h>
 
+#define MAXLINE 256
 #define READ 0
 #define WRITE 1
 
+char* getTimeToString() {
+	time_t currentTime;
+
+	if ((currentTime = time(NULL)) == (time_t) - 1) 
+		printf("Error getting current time.");
+
+	char str[20];
+	sprintf(str, "%02d-%02d-%02dT%02d:%02d:%02d", 
+		localtime(&currentTime)->tm_year+1900, 
+		localtime(&currentTime)->tm_mon+1, 
+		localtime(&currentTime)->tm_mday, 
+		localtime(&currentTime)->tm_hour, 
+		localtime(&currentTime)->tm_min, 
+		localtime(&currentTime)->tm_sec);
+
+	char* timeStr; 
+	if((timeStr = malloc(strlen(str)+1)) != NULL){ 
+		timeStr[0] = '\0'; // ensures the memory is an empty string
+		strcat(timeStr, str); 
+	} else { 
+		printf("malloc failed!\n");
+	}
+
+	return timeStr;
+}
+
 int monitorAux(char* search, char* filename) {
-	int p1[2], p2[2];
+	int n, p1[2], p2[2];
 	pid_t pidTail, pidGrep;
+	char line[MAXLINE];
 
 	// creating pipes
 	if (pipe(p1) < 0 || pipe(p2) < 0) {
@@ -27,6 +57,10 @@ int monitorAux(char* search, char* filename) {
 		} else if (pidGrep > 0) {
 			// parent running
 			close(p2[WRITE]);
+			printf("Ola");
+			n = read(p2[READ], line, MAXLINE);
+
+			write(STDOUT_FILENO, line, n);
 		} else {
 			// son that runs grep
 			close(p1[WRITE]);

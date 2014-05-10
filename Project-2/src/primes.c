98bcebed39fbf40c419b7c8db9a477b18f534243
Henrique Ferrolho <henriqueferrolho@gmail.com>
2014-05-10 23:52:22 +0100

    Starting to try circular queues

--- Project-2/src/primes.c
+++ Project-2/src/primes.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+#include <math.h>
 #include <unistd.h>
+/*
 #include <signal.h>
 #include <string.h>
 #include <sys/wait.h>
@@ -11,6 +12,34 @@
 
 #define QUEUE_SIZE 10
 
+int writeID;
+int* primesList;
+int primesListSize;
+
+int compare (const void * a, const void * b) {
+	return *(int*)a - *(int*)b;
+}
+
+void printPrimesList() {
+	int i;
+	for (i = 0; i < primesListSize; i++)
+		printf("%d ", primesList[i]);
+	printf("\n");
+}
+
+void addNumToPrimesList(int num) {
+	primesList[writeID++] = num;
+	primesListSize++;
+}
+
+void* initThreadFunc(void *arg) {
+	printf("> Starting initial thread\n");
+
+	addNumToPrimesList(2);
+
+	return NULL;
+}
+
 int main(int argc, char** argv) {
 	CircularQueue *q;
 	queue_init(&q, QUEUE_SIZE);
@@ -18,7 +47,7 @@ int main(int argc, char** argv) {
 	// validating number of arguments
 	if (argc <= 1) {
 		printf("Wrong number of arguments.\n");
-		printf("Usage: primes <N>\n");
+		printf("Usage: primes <n>\n");
 		return -1;
 	}
 
@@ -29,16 +58,35 @@ int main(int argc, char** argv) {
 	// validating input
 	if (argv[1] == pEnd || n < 2) {
 		printf("Invalid argument.\n");
-		printf("<N> must be an integer greater or equal than 2.\n");
+		printf("<n> must be an integer greater or equal than 2.\n");
 		return -1;
 	}
 
 	// Debugging block
 	printf("------------\n");
 	printf("Primes info:\n");
-	printf("N: %ld\n", n);
+	printf("n: %ld\n", n);
 	printf("------------\n");
 
-	printf("Done!\n");
+	// initializing shared data
+	writeID = 0;
+	int allocationSize = 1.2 * n / log(n);
+	primesList = (int*) malloc(allocationSize * sizeof(int));
+	primesListSize = 0;
+
+	// starting initial thread
+	pthread_t initThread;
+	pthread_create(&initThread, NULL, initThreadFunc, argv[1]);
+	pthread_join(initThread,NULL);
+
+	// sorting primes list
+	qsort(primesList, primesListSize, sizeof(int), compare);
+
+	printf("\n");
+	printf("The list of primes between 2 and %ld is:\n", n);
+	printPrimesList();
+	printf("\n");
+
+	printf("Program: Done!\n");
 	return 0;
 }

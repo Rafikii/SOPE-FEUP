27cb8fe98aec5487f85f42ad80fd2be89a90d3ee
Henrique Ferrolho <henriqueferrolho@gmail.com>
2014-05-15 19:33:13 +0100

    Fixed some bugs: freeing queues memory; added mutex when writing to primes list; added more usage arguments; ensured that termination semaphore only gets signaled after the zero from the circular queue is received.

--- Project-2/src/primes.c
+++ Project-2/src/primes.c
@@ -5,33 +5,37 @@
 
 #include "CircularQueue.h"
 
-#define DEBUG_MODE 0
-#define QUEUE_SIZE 10
+int DEBUG_MODE = 0;
+int QUEUE_SIZE = 10;
 
-int writeID;
-int* primesList;
+int writeIndex;
+unsigned long* primesList;
 int primesListSize;
 
-// TODO can this be global?
 long n;
 sem_t termSem;
+pthread_mutex_t primesListAccessControlMutex;
 
 int compare(const void * a, const void * b) {
-	return *(int*) a - *(int*) b;
+	return *(unsigned long*) a - *(unsigned long*) b;
 }
 
 void addNumToPrimesList(int num) {
-	primesList[writeID++] = num;
+	pthread_mutex_lock(&primesListAccessControlMutex);
+
+	primesList[writeIndex++] = num;
 	primesListSize++;
 
 	if (DEBUG_MODE)
 		printf("Added %d to primes list.\n", num);
+
+	pthread_mutex_unlock(&primesListAccessControlMutex);
 }
 
 void printPrimesList() {
 	int i;
 	for (i = 0; i < primesListSize; i++)
-		printf("%d ", primesList[i]);
+		printf("%ld ", primesList[i]);
 	printf("\n");
 }
 
@@ -48,7 +52,7 @@ void* filterThread(void* arg) {
 			num = queue_get(inputCircularQueue);
 		} while (num != 0);
 
-		// signaling semaphore
+		// signaling termination semaphore
 		if (DEBUG_MODE)
 			printf("Signaling semaphore: num > sqrt(n)\n");
 		sem_post(&termSem);
@@ -67,20 +71,28 @@ void* filterThread(void* arg) {
 			// reading next element from the circular queue
 			num = queue_get(inputCircularQueue);
 
-			if (num % temp != 0 || num == 0)
+			if (num % temp != 0)
 				queue_put(outputCircularQueue, num);
 		} while (num != 0);
 
 		// adding first number to primes list
 		addNumToPrimesList(temp);
+
+		// placing num (that by now should be equal to zero) at the end of the queue to terminate the sequence
+		queue_put(outputCircularQueue, num);
 	}
+
+	// destroying input circular queue
+	queue_destroy(inputCircularQueue);
 
 	return NULL ;
 }
 
 void* initThreadFunc(void* arg) {
-	printf("> Starting initial thread\n");
+	if (DEBUG_MODE)
+		printf("> Starting initial thread\n");
 
+	// adding number 2 to primes list
 	addNumToPrimesList(2);
 
 	if (n > 2) {
@@ -94,14 +106,13 @@ void* initThreadFunc(void* arg) {
 
 		// placing odd numbers in the output circular queue
 		int i;
-		for (i = 3; i <= n; i += 2) {
+		for (i = 3; i <= n; i += 2)
 			queue_put(outputCircularQueue, i);
-		}
 
-		// placing a 0 at the end of the queue to terminate the sequence
+		// placing zero at the end of the queue to terminate the sequence
 		queue_put(outputCircularQueue, 0);
 	} else {
-		// signaling semaphore
+		// signaling termination semaphore
 		if (DEBUG_MODE)
 			printf("Signaling semaphore: n = 2\n");
 		sem_post(&termSem);
@@ -111,56 +122,106 @@ void* initThreadFunc(void* arg) {
 }
 
 int main(int argc, char** argv) {
+	char* pEnd;
+
 	// validating number of arguments
 	if (argc <= 1) {
+		printf("\n");
 		printf("Wrong number of arguments.\n");
 		printf("Usage: primes <n>\n");
+		printf("Usage: primes <n> <queue size>\n");
+		printf("Usage: primes <n> <queue size> <debug mode>\n");
+		printf("\n");
+
 		return -1;
 	}
 
-	// processing input
-	char* pEnd;
+	// processing and validating <debug mode> paramater
+	if (argc == 4) {
+		DEBUG_MODE = strtol(argv[3], &pEnd, 10);
+		if (argv[3] == pEnd || (DEBUG_MODE != 0 && DEBUG_MODE != 1)) {
+			printf("\n");
+			printf("Invalid argument.\n");
+			printf("<debug mode> must be 0 or 1.\n");
+			printf("\n");
+
+			return -1;
+		}
+
+		if (DEBUG_MODE)
+			printf("DEBUG_MODE value overrided to: %d\n", DEBUG_MODE);
+	}
+
+	// processing and validating <queue size> paramater
+	if (argc >= 3) {
+		QUEUE_SIZE = strtol(argv[2], &pEnd, 10);
+		if (argv[2] == pEnd || QUEUE_SIZE <= 0) {
+			printf("\n");
+			printf("Invalid argument.\n");
+			printf("<queue size> must be an integer greater than 0.\n");
+			printf("\n");
+
+			return -1;
+		}
+
+		if (DEBUG_MODE)
+			printf("QUEUE_SIZE value overrided to: %d\n", QUEUE_SIZE);
+	}
+
+	// processing and validating <n> paramater
 	n = strtol(argv[1], &pEnd, 10);
-
-	// validating input
 	if (argv[1] == pEnd || n < 2) {
+		printf("\n");
 		printf("Invalid argument.\n");
-		printf("<n> must be an integer greater or equal than 2.\n");
+		printf("<n> must be an integer greater than or equal to 2.\n");
+		printf("\n");
+
 		return -1;
 	}
 
-	// Debugging block
-	printf("------------\n");
-	printf("Primes info:\n");
-	printf("n: %ld\n", n);
-	printf("------------\n");
+	// displaying received data info
+	if (DEBUG_MODE) {
+		printf("---------------\n");
+		printf("Primes info:\n");
+		printf("n: %ld\n", n);
+		printf("queue size: %d\n", QUEUE_SIZE);
+		printf("debug mode: %d\n", DEBUG_MODE);
+		printf("---------------\n");
+	}
 
 	// initializing shared data
-	writeID = 0;
+	writeIndex = 0;
 	int allocationSize = 1.2 * n / log(n);
-	primesList = (int*) malloc(allocationSize * sizeof(int));
+	primesList = (unsigned long*) malloc(allocationSize * sizeof(unsigned long));
 	primesListSize = 0;
 
 	// initializing termination semaphore
 	if (sem_init(&termSem, 0, 0) != 0)
 		return -1;
 
+	// initializing primes list access control mutex
+	if (pthread_mutex_init(&primesListAccessControlMutex, NULL) != 0)
+		return -1;
+
 	// starting initial thread
 	pthread_t initThread;
-	pthread_create(&initThread, NULL, initThreadFunc, NULL );
-	pthread_join(initThread, NULL );
+	pthread_create(&initThread, NULL, initThreadFunc, NULL);
 
-	// waiting for semaphore
+	// waiting for termination semaphore
 	sem_wait(&termSem);
 
 	// sorting primes list
-	qsort(primesList, primesListSize, sizeof(int), compare);
+	if (DEBUG_MODE)
+		printf("Sorting primes list\n");
+	qsort(primesList, primesListSize, sizeof(unsigned long), compare);
 
+	// displaying primes list
 	printf("\n");
 	printf("The list of primes between 2 and %ld is:\n", n);
 	printPrimesList();
 	printf("\n");
 
-	printf("Program: Done!\n");
+	if (DEBUG_MODE)
+		printf("Program Terminated.\n");
 	return 0;
 }
